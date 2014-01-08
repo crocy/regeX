30582f35f63dc7acdb5bab31c7f8e65f1c0c639f
Nejc Krokter <nejc.krokter@outfit7.com>
2014-01-08 01:51:39 +0100

    Added arrow down animation on exercise view enter to suggest to the user what to do.

--- regeX/src/main/java/com/crocx/regex/exercises/view/ExerciseView.java
+++ regeX/src/main/java/com/crocx/regex/exercises/view/ExerciseView.java
@@ -5,9 +5,11 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.crocx.regex.R;
@@ -18,7 +20,7 @@ import com.crocx.regex.ui.UiStateManager;
 /**
  * Created by Croc on 10.11.2013.
  */
-public class ExerciseView extends LinearLayout {
+public class ExerciseView extends RelativeLayout {
 
     private static final int COLOR_RESULT_MATCH_TRUE = 0xFF00FF00;
     private static final int COLOR_RESULT_MATCH_FALSE = 0xFFFF0000;
@@ -30,6 +32,7 @@ public class ExerciseView extends LinearLayout {
     private TextView result;
     private TextView resultError;
     private EditText input;
+    private View animationArrow;
 
     public ExerciseView(Context context) {
         super(context);
@@ -37,6 +40,10 @@ public class ExerciseView extends LinearLayout {
 
     public ExerciseView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public ExerciseView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
     }
 
     @Override
@@ -47,6 +54,7 @@ public class ExerciseView extends LinearLayout {
         result = (TextView) findViewById(R.id.exerciseResult);
         resultError = (TextView) findViewById(R.id.exerciseResultError);
         input = (EditText) findViewById(R.id.exerciseInput);
+        animationArrow = findViewById(R.id.exerciseAnimationArrow);
     }
 
     public void init(final UiStateManager uiStateManager) {
@@ -73,6 +81,8 @@ public class ExerciseView extends LinearLayout {
         });
 
         resultError.setTextColor(COLOR_RESULT_MATCH_FALSE);
+
+        animationArrow.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.exercise_arrow_down_animation));
     }
 
     public void updateView(ExerciseItem exerciseItem) {
@@ -83,13 +93,10 @@ public class ExerciseView extends LinearLayout {
 
         contentWebView.loadUrl(exerciseItem.getInstructionsUrl());
 
-        if (exerciseItem.isPreferSolutionOutput()) {
-            result.setText(exerciseItem.getSolutionOutput());
-        } else {
-            result.setText(exerciseItem.getSolutionRegex());
-        }
+        resultError.setText(null);
+        resultError.setVisibility(GONE);
 
-        input.setText("");
+        input.setText(""); // clean possible previous input
     }
 
     public void updateRegexResult(String match, boolean matches) {
@@ -122,11 +129,14 @@ public class ExerciseView extends LinearLayout {
 
     private void checkInputAndFireAction(UiStateManager uiStateManager) {
         if (input.getText() != null && input.getText().toString().trim().length() > 0) {
+            result.setVisibility(VISIBLE);
             uiStateManager.fireAction(ExercisesAction.EVALUATE_REGEX, input.getText().toString());
             input.setHint(null);
         } else {
-            input.setHint(R.string.exercise_hint_input_cant_be_empty);
+            //            input.setHint(R.string.exercise_hint_input_cant_be_empty);
+            input.setHint(R.string.exercise_hint_input_here);
             updateRegexResult("", false);
+            result.setVisibility(GONE);
         }
     }
 
